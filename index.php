b8ce6b15995b01d8aadd38c2e2f32dd0d14953f0
Malte <malte@yootheme.com>
2016-03-11 14:20:01 +0100

    update

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ return [
 
         'tinymce/{locale}.js' => [
 
-            'name' => '@tinymce/intl.js',
+            'name' => '@tinymce/intl',
             'controller' => function ($locale) use ($app) {
 
                 $langFile = '';
